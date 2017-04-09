bf511b0af1de191abe577f48fee763b95ed85b1a
hao.li.peng@163.com <hao.li.peng@163.com>
2017-04-09 15:12:46 +0800

    ss

--- myserver/main.go
+++ myserver/main.go
@@ -7,15 +7,13 @@ import (
 	"fmt"
 )
 
-var server = &http.Server{}
 
-// hello world, the web server
-func HelloServer(w http.ResponseWriter, req *http.Request) {
+func helloHandle(w http.ResponseWriter, req *http.Request) {
 	fmt.Println(req.RequestURI, "hello, world!")
 	io.WriteString(w, "hello, world!\n")
 }
 
 func main() {
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/", helloHandle)
 	log.Fatal(http.ListenAndServe(":12345", nil))
 }

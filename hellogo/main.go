03e354727f978fd3507cff51fed5d2eb5382bd4e
hao.li.peng@163.com <hao.li.peng@163.com>
2017-05-18 23:58:00 +0800

    sdfs

--- hellogo/main.go
+++ hellogo/main.go
@@ -1,11 +1,56 @@
 package main
 
 import (
-	"github.com/astaxie/beego"
-	_ "hellogo/routers"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"io/ioutil"
 )
 
-
 func main() {
-	beego.Run()
+	http.HandleFunc("/", index)
+	http.HandleFunc("/upload", upload)
+	http.ListenAndServe(":1789", nil)
 }
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("uploadfile")
+
+	fmt.Println(r.Header.Get("Content-Type"))
+	fmt.Println(ioutil.ReadAll(r.Body))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	f, err := os.OpenFile(handler.Filename, os.O_WRONLY | os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+	fmt.Fprintln(w, "upload ok!",handler.Filename)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(tpl))
+}
+
+const tpl = `<html>  
+<head>  
+<title>上传文件</title>  
+</head>  
+<body>  
+<form enctype="multipart/form-data" action="/upload" method="post">  
+ <input type="file" name="uploadfile" />  
+ <input type="hidden" name="token" value="{...{.}...}"/>  
+ <input type="submit" value="upload" />  
+</form>  
+</body>  
+</html>`  
+
--- hellogo/tests/beego_test.go
+++ hellogo/tests/beego_test.go
@@ -6,31 +6,62 @@ import (
 	"testing"
 	"github.com/astaxie/beego"
 	"fmt"
+	"bytes"
+	"mime/multipart"
+	"net/http"
+	"io/ioutil"
 )
 
 func init() {
 	_, file, _, _ := runtime.Caller(1)
-	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".."+string(filepath.Separator))))
-	apppath = filepath.Join(apppath,"conf","app_win.conf")
+	apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".." + string(filepath.Separator))))
+	apppath = filepath.Join(apppath, "conf", "app_win.conf")
 	//beego.BeeApp.LoadAppConfig("ini",apppath)
 	//beego.TestBeegoInit(apppath)
 }
 
 // TestBeego is a sample to run an endpoint test
+func TestBeegoUpload(t *testing.T) {
+	var buffer bytes.Buffer
+
+	w := multipart.NewWriter(&buffer)
+	//  Write fields and files w.CreateFormField( " input1 " )
+	w.WriteField("input1", "value1")
+
+	write,err :=ioutil.TempFile("c:/","rnd1.txt")
+	fmt.Println(err)
+
+	write.WriteString("ssss")
+	write.Close()
+
+	w.CreateFormFile("file", "c:/rnd.txt")
+
+	fmt.Println(w.FormDataContentType())
+	fmt.Println(string(buffer.Bytes()))
+
+	resp, err := http.Post("http://www.baidu.com/", w.FormDataContentType(), &buffer)
+	fmt.Println(err)
+	data, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(data))
+}
+func TestBeegoHandle(t *testing.T) {
+
+}
+
+// TestBeego is a sample to run an endpoint test
 func TestBeego(t *testing.T) {
 	var value string
-	value =beego.AppConfig.String("appname")
+	value = beego.AppConfig.String("appname")
 	fmt.Println(value)
 
-	beego.AppConfig.Set("appname","newappname")
-	value =beego.AppConfig.String("appname")
+	beego.AppConfig.Set("appname", "newappname")
+	value = beego.AppConfig.String("appname")
 	fmt.Println(value)
 
-	value =beego.AppConfig.String("myset")
+	value = beego.AppConfig.String("myset")
 	fmt.Println(value)
 
-	beego.AppConfig.Set("myset","newsetvalue")
-	value =beego.AppConfig.String("myset")
+	beego.AppConfig.Set("myset", "newsetvalue")
+	value = beego.AppConfig.String("myset")
 	fmt.Println(value)
-
 }
